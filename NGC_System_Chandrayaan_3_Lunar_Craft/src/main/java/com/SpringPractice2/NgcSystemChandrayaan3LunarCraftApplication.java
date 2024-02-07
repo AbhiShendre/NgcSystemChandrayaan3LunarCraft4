17c911f24f7d1fadb086d872fa86cefdf433476f
AbhiShendre <aclasic12345@gmail.com>
2024-02-07 19:28:10 +0530

    Build Project

---
+++ NGC_System_Chandrayaan_3_Lunar_Craft/src/main/java/com/SpringPractice2/NgcSystemChandrayaan3LunarCraftApplication.java
@@ -0,0 +1,13 @@
+package com.SpringPractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgcSystemChandrayaan3LunarCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgcSystemChandrayaan3LunarCraftApplication.class, args);
+	}
+
+}
---
+++ NGC_System_Chandrayaan_3_Lunar_Craft/src/test/java/com/SpringPractice2/NgcSystemChandrayaan3LunarCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringPractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgcSystemChandrayaan3LunarCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
